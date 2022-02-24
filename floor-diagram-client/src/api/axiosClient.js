440ea8fde1ad500606e1127ebfa133e84bb40600
haosmall <nhathao00852@gmail.com>
2022-02-24 11:55:57 +0700

    update baseURL in axiosClient.js

--- floor-diagram-client/src/api/axiosClient.js
+++ floor-diagram-client/src/api/axiosClient.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-	baseURL: `${process.env.REACT_APP_API_URL}/api`,
+	baseURL: "http://localhost:5000/api",
+	// baseURL: `${process.env.REACT_APP_API_URL}/api`,
 	headers: {
 		"content-type": "application/json",
 	},
