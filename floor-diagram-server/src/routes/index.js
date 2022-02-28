0c386ec321ea86384776e12b998032373ecbd401
haosmall <nhathao00852@gmail.com>
2022-02-28 10:11:52 +0700

    ...

--- floor-diagram-server/src/routes/index.js
+++ floor-diagram-server/src/routes/index.js
@@ -9,13 +9,13 @@ const teamRouter = require("./team");
 const projectRouter = require("./project");
 
 const route = (app) => {
-  app.use("/api/admin", adminRouter());
-  app.use("/api/buildings", isAdmin, buildingRouter());
-  app.use("/api/employees", isAdmin_or_buildingAdmin, employeeRouter());
-  app.use("/api/floors", isAdmin_or_buildingAdmin, floorRouter());
-  app.use("/api/rooms", isAdmin_or_buildingAdmin, roomRouter());
-  app.use("/api/groups", isAdmin_or_buildingAdmin, groupRouter());
-  app.use("/api/teams", isAdmin_or_buildingAdmin, teamRouter());
-  app.use("/api/projects", isAdmin_or_buildingAdmin, projectRouter());
+	app.use("/api/admin", adminRouter());
+	app.use("/api/buildings", isAdmin, buildingRouter());
+	app.use("/api/employees", isAdmin_or_buildingAdmin, employeeRouter());
+	app.use("/api/floors", isAdmin_or_buildingAdmin, floorRouter());
+	app.use("/api/rooms", isAdmin_or_buildingAdmin, roomRouter());
+	app.use("/api/groups", isAdmin_or_buildingAdmin, groupRouter());
+	app.use("/api/teams", isAdmin_or_buildingAdmin, teamRouter());
+	app.use("/api/projects", isAdmin_or_buildingAdmin, projectRouter());
 };
 module.exports = route;
