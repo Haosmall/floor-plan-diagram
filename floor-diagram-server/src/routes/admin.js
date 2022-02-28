a385e53b2aaa14f86d26e2204617c3731630042f
Cuong Tran Hoang <thcuong1999@gmail.com>
2022-02-28 09:53:32 +0700

    update seeder mongoose

--- floor-diagram-server/src/routes/admin.js
+++ floor-diagram-server/src/routes/admin.js
@@ -3,7 +3,6 @@ const adminController = require("../controllers/AdminController");
 const router = express.Router();
 
 const adminRouter = () => {
-  router.get("", (req, res, next) => res.send("hello"));
   router.post("/registry", adminController.registry);
   router.post("/login", adminController.login);
 
