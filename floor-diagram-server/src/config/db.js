5632156f8599bf8c2c7cecb92e3c039458b82ef1
Cuong Tran Hoang <thcuong1999@gmail.com>
2022-02-28 09:05:42 +0700

    add seeder

--- floor-diagram-server/src/config/db.js
+++ floor-diagram-server/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { dirname, join } = require("path");
 require("dotenv").config({
-	path: join(__dirname, "..", "..", ".env"),
+  path: join(__dirname, "..", "..", ".env"),
 });
 
 const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
@@ -9,21 +9,21 @@ const DATABASE_PORT = process.env.DATABASE_PORT || 27017;
 const DATABASE_NAME = process.env.DATABASE_NAME || "floor-diagram-db";
 
 const MONGODB_URL =
-	process.env.MONGODB_URL || "mongodb://localhost:27017/floor-diagram-db";
+  process.env.MONGODB_URL || "mongodb://localhost:27017/floor-diagram-db";
 
 const connect = async () => {
-	try {
-		await mongoose.connect(
-			`mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`,
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			}
-		);
-		console.log("connect successfully");
-	} catch (error) {
-		console.error("connect fail", error);
-	}
+  try {
+    await mongoose.connect(
+      `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Databased connected successfully");
+  } catch (error) {
+    console.error("connect fail", error);
+  }
 };
 
 module.exports = { connect };
